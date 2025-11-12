0230d6c05912bae6d66a15cf28c91aba6331974a
Naim Abbud <43539101+NaimAbb@users.noreply.github.com>
2025-11-12 20:46:43 +0200

    add drawer menu

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,16 @@ export default function Home() {
       <HomeProjectsSection />
       <HomeContactUsSection />
 
-      <section className="container flex items-center justify-between">
-        <h2 className="text-[#DEDEDE] text-[63px] font-medium">
+      <section className="container flex flex-col gap-4 lg:flex-row items-center justify-between mb-12 lg:mb-0 text-center lg:text-start">
+        <h2 className="text-[#DEDEDE] text-[20px] lg:text-[63px] font-medium">
           Let’s
           <br />
           Work Together -{" "}
         </h2>
-        <a href="mailto:naim.abbud@gmail.com" className="flex items-center text-[22px] gap-3 text-[#C9C9C9] font-normal border border-[#484E53] rounded-2xl px-8 py-3">
+        <a
+          href="mailto:naim.abbud@gmail.com"
+          className="flex flex-col lg:flex-row items-center text-[16px] lg:text-[22px] gap-3 text-[#C9C9C9] font-normal border border-[#484E53] rounded-2xl px-8 py-3 max-w-full"
+        >
           <Image src={gmailIcon} alt="d" />
           naim.abbud@gmail.com
         </a>
